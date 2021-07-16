eef03c956e84f285be24f1fdca34a4d0c92601a6
blakeromano19 <blakeromano19@gmail.com>
2021-07-16 17:19:11 -0400

    search bar functional

--- controllers/companies-controller.js
+++ controllers/companies-controller.js
@@ -1,4 +1,5 @@
 import { Company } from "../models/company.js"
+import { Job } from "../models/job.js"
 
 export {
     newCompany as new,
@@ -8,6 +9,7 @@ export {
     edit,
     update,
     create,
+    search,
 }
 
 function newCompany (req, res) {
@@ -28,18 +30,52 @@ function index (req, res) {
     .catch(err => console.log(err))
 }
 function deleteCompany (req, res) {
+    const id = req.params.id
 
+    Company.findByIdAndDelete(id)
+    .then(result => {
+        res.redirect("/companies")
+    })
+    .catch(err => console.log(err))
 }
 
 function edit (req, res) {
-
+    Company.findById(req.params.id)
+    .then((company) =>{
+        res.render("companies/edit", { title: "Update Company Info", company: company, user: req.user ? req.user: null})
+    })
+    .catch(err => console.log(err))
 }
 function update (req, res) {
-
+    req.body.userPosted = req.user.profile
+    Company.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(company => {
+        res.redirect(`/companies/${company._id}`)
+    })
+    .catch(err => console.log(err))
 }
 function create (req, res) {
+    req.body._id = req.params.id
+    req.body.userPosted = req.user.profile
     const company = new Company(req.body)
     company.save()
     .then(result => res.redirect("/companies"))
     .catch(err => console.log(err))
+}
+function search(req, res) {
+    console.log(req.body.searchParam)
+    console.log(req.body.searchContent)
+    if (req.body.searchParam === "jobs") {
+        Job.find({ title: req.body.searchContent})
+        .then(jobs => {
+            console.log(jobs)
+            res.render("jobs/search", { title: "Search Results", jobs: jobs, user: req.user ? req.user: null})
+        })
+    } else if (req.body.searchParam === "companies") {
+        Company.find({ companyName: req.body.searchContent})
+        .then(companies => {
+            res.render("companies/search", { title: "Search Results", companies: companies, user: req.user ? req.user: null})
+        })
+        .catch(err => console.log(err))
+    }
 }
